686209e89e726c656694352e793c3b3c43002e63
amit <Amit741993@gmail.com>
2019-11-13 20:45:49 +0300

    Class 3: check string if the string (code with {} [] ()) is ok

---
+++ app/src/main/java/com/example/class31/Logic.java
@@ -0,0 +1,18 @@
+package com.example.class31;
+
+public class Logic {
+
+    /**
+     * check code string validation
+     * @param code - the string to check
+     * @return
+     */
+
+    public static boolean chkAnswer(String code){
+        // TODO: 11/13/2019 generate function that checks the string
+
+        return false;
+    }
+
+
+}
--- app/src/main/java/com/example/class31/MainActivity.java
+++ app/src/main/java/com/example/class31/MainActivity.java
@@ -4,17 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText main_EDT_input;
+    private Button main_BTN_chk;
+    private TextView main_LBL_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main_EDT_input = findViewById(R.id.main_EDT_input);
+        main_BTN_chk = findViewById(R.id.main_BTN_chk);
+        main_LBL_info = findViewById(R.id.main_LBL_info);
+
+        main_BTN_chk.setOnClickListener(myListener);
+
     }
 
+    View.OnClickListener myListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            start();
+        }
+    };
 
-    void print(){
-        Log.i("lala", "Heyyyy");
+    private void start(){
+        String input = main_EDT_input.getText().toString();
+        boolean answer = Logic.chkAnswer(input);
+        main_LBL_info.setText(answer+"");
     }
 }
